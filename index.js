1fb646e6cb4499b3097bda020e67ae66df71d1be
CC <apathicss@gmail.com>
2021-09-28 01:37:34 -0400

    secret info protection

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const config = require('./config/key')
+const { User } = require("./models/User")
+
+// application/x-www-form-urlencoded
+app.use(express.urlencoded({extended: true}))
+
+// application/json
+app.use(express.json())
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://chloecho:abcd1234@cluster0.lcf3u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
-  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
-}).then(() => console.log('MongoDB Connected...'))
+mongoose.connect(config.mongoURI)
+  .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/register', (req, res) => {
+
+  // get information for registering from client, then put into the database
+  const user = new User(req.body)
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err })
+    return res.status(200).json({
+      success: true
+    })
+  })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
